49bd352c0d333263b9a3d2da59d420a5a5c8633c
Brian Otieno <brianokola@gmail.com>
2020-04-18 19:12:52 +0300

    Added a method to save a new user account into a list

--- passlock.py
+++ passlock.py
@@ -8,9 +8,17 @@ class User:
     Create User class that gnerates new instances for uer.
     """
 
+    user_list = []
+
     def __init__(self, username, password):
         """
         Method taht defines the properties of a user
         """
         self.username = username
         self.password = password
+
+    def save_user(self):
+        """
+        Method to save a new user into the user list
+        """
+        User.user_list.append(self)
